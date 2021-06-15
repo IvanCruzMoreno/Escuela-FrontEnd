e9840574a3b12f6e62a804ba0d7256bf54022774
ivan <ivanmoreno405@gmail.com>
2021-06-15 16:54:57 -0500

    We added alumnos, cursos and examenes components

---
+++ src/app/components/alumnos/alumnos.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnos',
+  templateUrl: './alumnos.component.html',
+  styleUrls: ['./alumnos.component.css']
+})
+export class AlumnosComponent implements OnInit {
+
+  titulo: string = 'Lista de alumnos';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
