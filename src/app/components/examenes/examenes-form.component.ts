2f5a72e006cb9cccd3117d3c9e64d9486cc7501e
ivan <ivanmoreno405@gmail.com>
2021-06-23 11:39:07 -0500

    select asignatura Padre and Hija in Examenes-form

--- src/app/components/examenes/examenes-form.component.ts
+++ src/app/components/examenes/examenes-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Asignatura } from 'src/app/models/asignatura';
 import { Examen } from 'src/app/models/examen';
 import { ExamenService } from 'src/app/services/examen.service';
 import { CommonFormComponent } from '../common-form.component';
@@ -11,6 +12,9 @@ import { CommonFormComponent } from '../common-form.component';
 })
 export class ExamenesFormComponent extends CommonFormComponent<Examen, ExamenService> implements OnInit {
 
+  asignaturasPadre: Asignatura[] = [];
+  asignaturasHija: Asignatura[] = [];
+
   constructor(service: ExamenService, 
     router: Router,
     route: ActivatedRoute) { 
@@ -19,6 +23,38 @@ export class ExamenesFormComponent extends CommonFormComponent<Examen, ExamenSer
       this.entity = new Examen();
       this.nombreEntity = Examen.name;
       this.redirect = '/examenes';
-    }
+  }
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( params => {
+      const id: number = +params.get('id');
+      if(id){
+        this.service.ver(id).subscribe(respuesta => {
+          this.entity = respuesta;
+        });
+        this.titulo = `Editar ${this.nombreEntity}`;
+
+        this.service.findAllAsignatura().subscribe( asignaturaRespuesta => {
+          this.asignaturasHija = asignaturaRespuesta.filter( asignaturaRespuesta => 
+            asignaturaRespuesta.padre && asignaturaRespuesta.padre.id === this.entity.asignaturaPadre.id);
+        });
+      }
+    });
+
+    this.service.findAllAsignatura().subscribe( asignaturasRespuesta => {
+      this.asignaturasPadre = asignaturasRespuesta.filter( asignaturaRespuesta => 
+        !asignaturaRespuesta.padre);
+    });
+  }
+
+  cargarHijos(): void {
+    this.asignaturasHija = this.entity.asignaturaPadre? this.entity.asignaturaPadre.hijos : [];
+  }
+
+  compararAsignatura(a1: Asignatura, a2: Asignatura): boolean{
+    if(a1 === undefined && a2 === undefined){
+      return true;
+    }
+    return (a1 === null || a1 === undefined || a2 === null || a2 === undefined)? false: a1.id === a2.id;
+  }
 }
--- src/app/services/examen.service.ts
+++ src/app/services/examen.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { BASE_ENDPOINT } from '../config/app';
+import { Asignatura } from '../models/asignatura';
 import { Examen } from '../models/examen';
 import { CommonService } from './common.service';
 
@@ -13,5 +15,9 @@ export class ExamenService extends CommonService<Examen>{
 
   constructor(http: HttpClient) { 
     super(http);
+  }
+
+  public findAllAsignatura(): Observable<Asignatura[]> {
+    return this.http.get<Asignatura[]>(`${this.baseEndPoint}/asignaturas`);
   }
 }
